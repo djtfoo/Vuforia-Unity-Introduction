e9fef864f9a96c379397fd29ebf5cf4b6dc7f336
JT <djt.foo@gmail.com>
2019-02-27 11:24:03 +0800

    Able to simulate tracking on/off via button press, and camera moves to the marker. Camera can move and rotate via user controls

--- Assets/VuforiaSimulator/CustomTrackableEventHandler.cs
+++ Assets/VuforiaSimulator/CustomTrackableEventHandler.cs
@@ -5,10 +5,12 @@ Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
 All Rights Reserved.
 Confidential and Proprietary - Protected under copyright and other laws.
 
+* Class: CustomTrackableEventHandler
 * A generic Vuforia tracking event handler implementing the ITrackableEventHandler interface.
 * Also allows testing of codes without requiring actual tracking,
 *  via pressing a button to simulate tracking found or lost.
 * Modified By: Foo Jing Ting
+* Date: 27 February 2019
 ==============================================================================*/
 
 using UnityEngine;
@@ -40,12 +42,12 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
     #endregion // PRIVATE_MEMBER_VARIABLES
 
     #region VARIABLES_FOR_TESTING_WITHOUT_CAMERA_OR_MARKER
-    [SerializeField]
     [Tooltip("To simulate having tracked the marker, and move it in front of ARCamera")]
-    protected bool enableTrackingWithoutMarker = false;
-    [SerializeField]
+    public bool enableTrackingWithoutMarker = false;
     [Tooltip("Key to press to track/untrack this ImageTarget")]
-    protected KeyCode toggleTrackingKey;
+    public KeyCode toggleTrackingKey;
+    [Tooltip("Texture for this ImageTarget's image marker")]
+    public Texture imageTargetTexture;
 
     #endregion  // TESTING_WITHOUT_CAMERA_OR_MARKER_VARIABLES
 
@@ -61,33 +63,120 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
         OnTrackingBegin();
     }
 
-    protected void Update()
+    protected virtual void Update()
     {
+#if UNITY_EDITOR
         if (enableTrackingWithoutMarker)    // trigger tracking via user input is enabled
         {
             if (Input.GetKeyDown(toggleTrackingKey))    // trigger tracking via button press
             {
-                if (isFound)    // was already found
-                {
-                    OnTrackingLost();   // toggle tracking off
-                }
-                else    // previously not found yet
-                {
-                    OnTrackingFound();  // toggle tracking on
-                    
-                    // get position of ARCamera
-                    Transform camera = GameObject.Find("ARCamera").transform;
-                    // set position in front of camera, with scale of this ImageTarget to determine distance from camera
-                    transform.position = camera.position + camera.forward * Mathf.Max(transform.localScale.x, transform.localScale.z) - new Vector3(0f, 0.25f * transform.localScale.y, 0f);
-
-                    // for better precision, get bounds of all children objects of this ImageTarget
-
-                }
+                TriggerTrackingToggle();
             }
         }
+#endif
     }
 
     #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+    #region SIMULATION_HELPER_METHODS
+
+    /// <summary>
+    ///     Toggle between tracking and not tracking.
+    ///     Can be called by other function callbacks and not just in Update().
+    /// </summary>
+    public void TriggerTrackingToggle()
+    {
+        if (isFound)    // was already found
+        {
+            OnTrackingLost();   // toggle tracking off
+        }
+        else    // previously not found yet
+        {
+            OnTrackingFound();  // toggle tracking on
+
+            // recreate image marker
+            if (GetComponent<Renderer>() == null)   // no marker yet
+            {
+                CreateImageTarget();
+            }
+
+            // get position of ARCamera
+            Transform camera = GameObject.Find("ARCamera").transform;
+            // set position in front of camera, with scale of this ImageTarget to determine distance from camera
+            //transform.position = camera.position + camera.forward * Mathf.Max(transform.localScale.x, transform.localScale.z) - new Vector3(0f, 0.25f * transform.localScale.y, 0f);
+
+            // for better precision, get bounds of all children objects of this ImageTarget
+            Bounds bounds = getBoundsOfChildren(transform);
+
+            Vector3 boundsSize = bounds.size;
+            // place camera to view object
+            float distance = Mathf.Max(bounds.size.x, bounds.size.y, bounds.size.z);
+            distance /= (2.0f * Mathf.Tan(0.5f * camera.GetComponent<Camera>().fieldOfView * Mathf.Deg2Rad));
+            //transform.position = camera.position + distance * camera.forward; // place object in front of camera
+            camera.position = transform.position - distance * camera.forward;
+        }
+    }
+
+    /// <summary>
+    ///     Funcion to get encapsulated Bounds of all children GameObjects recursively.
+    /// </summary>
+    /// <returns> Bounds encapsulating all Collider and Mesh Bounds in the GameObject hierarchy </returns>
+    protected Bounds getBoundsOfChildren(Transform t)
+    {
+        // create total bounds of this GameObject's Mesh and Collider Bounds
+        Bounds totalBounds = new Bounds();
+        if (t.GetComponent<Renderer>()) // there is MeshFilter Component
+        {
+            totalBounds.Encapsulate(t.GetComponent<Renderer>().bounds);
+        }
+        if (t.GetComponent<Collider>())   // there is a Collider Component
+        {
+            totalBounds.Encapsulate(t.GetComponent<Collider>().bounds);
+        }
+
+        if (t.childCount > 0)
+        {
+            foreach (Transform child in t)
+            {
+                totalBounds.Encapsulate(getBoundsOfChildren(child));
+            }
+        }
+
+        return totalBounds;
+    }
+
+    protected void CreateImageTarget()
+    {
+        // attach Components
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.AddComponent<MeshFilter>();
+
+        // create Mesh
+        Vector2 size = GetComponent<ImageTargetBehaviour>().GetSize();
+        Vector3[] newVertices = { new Vector3(-0.5f,0f,-0.5f), new Vector3(0.5f,0f,-0.5f), new Vector3(0.5f,0f,0.5f), new Vector3(-0.5f,0f,0.5f) };
+        for (int i = 0; i < newVertices.Length; i++)
+        {
+            // actual vertices will be scaled by transform's localScale, so cancel it off by dividing
+            newVertices[i].x *= (size.x / transform.localScale.x);
+            newVertices[i].z *= (size.y / transform.localScale.x);
+        }
+        Vector2[] newUV = { new Vector2(0,0), new Vector2(1,0), new Vector2(1,1), new Vector2(0,1) };
+        int[] newTriangles = { 2, 1, 0,
+            3, 2, 0,
+            0, 1, 2,    // back side
+            0, 2, 3 };  // back side
+        Mesh mesh = new Mesh();
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        // create Material
+        GetComponent<Renderer>().material = new Material(Shader.Find("Unlit/Texture"));
+        GetComponent<Renderer>().material.mainTexture = imageTargetTexture;
+    }
+
+    #endregion  // SIMULATION_HELPER_METHODS
 
     #region PUBLIC_METHODS
 
@@ -196,7 +285,7 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 
     #endregion // PRIVATE_METHODS
 
-    #region HELPER_METHODS
+    #region TRACKING_HELPER_METHODS
 
     /// <summary>
     ///     Enable or disable rendering components
@@ -216,10 +305,10 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
         /*foreach (var component in colliderComponents)
             component.enabled = enable;*/
 
-        // Set canvas':
+        // Set canvas:
         foreach (var component in canvasComponents)
             component.enabled = enable;
     }
 
-    #endregion // HELPER_METHODS
+    #endregion // TRACKING_HELPER_METHODS
 }
---
+++ Assets/VuforiaSimulator/MoveCamera.cs
@@ -0,0 +1,72 @@
+﻿/*==============================================================================
+* Class: MoveCamera
+* Script to attach to the Camera to move it via key press and rotate by
+* moving mouse while holding down RMB.
+* Author: Foo Jing Ting
+* Date: 27 February 2019
+==============================================================================*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    [SerializeField]
+    private KeyCode keyForward = KeyCode.W;
+    [SerializeField]
+    private KeyCode keyBackward = KeyCode.S;
+    [SerializeField]
+    private KeyCode keyLeft = KeyCode.A;
+    [SerializeField]
+    private KeyCode keyRight = KeyCode.D;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    private float yawSpeed = 25f;
+    private float pitchSpeed = 25f;
+    
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+#if UNITY_EDITOR
+        // rotate camera while holding RMB
+        if (Input.GetMouseButton(1))    // RMB held down
+        {
+            if (Input.GetMouseButtonDown(1))    // frame in which it was set
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+            
+            Vector3 eulerAngles = transform.eulerAngles;
+            eulerAngles.x -= pitchSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+            eulerAngles.y += yawSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+            transform.eulerAngles = eulerAngles;
+        }
+        else if (Input.GetMouseButtonUp(1)) // frame in which RMB no longer pressed
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        // move via movement keys
+        if (Input.GetKey(keyForward)) {
+            transform.position += moveSpeed * transform.forward;
+        }
+        if (Input.GetKey(keyBackward)) {
+            transform.position -= moveSpeed * transform.forward;
+        }
+        if (Input.GetKey(keyLeft)) {
+            transform.position -= moveSpeed * transform.right;
+        }
+        if (Input.GetKey(keyRight)) {
+            transform.position += moveSpeed * transform.right;
+        }
+    }
+#endif
+}
